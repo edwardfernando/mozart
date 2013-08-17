76e66a716f05865d851a8f7690171706d26ed838
Edward Fernando <edward.fer@gmail.com>
2013-08-17 20:45:54 +0700

    Add wrapper for JAXB

---
+++ src/main/java/api/mozart/wrapper/InstrumentsWrapper.java
@@ -0,0 +1,35 @@
+package api.mozart.wrapper;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import api.mozart.model.Instrument;
+
+@XmlRootElement(name = "instrumentsList")
+public class InstrumentsWrapper {
+
+	private Date createdDate = new Date();
+	private List<Instrument> instruments = new ArrayList<Instrument>();
+
+	public List<Instrument> getInstruments() {
+		return instruments;
+	}
+
+	@XmlElement
+	public void setInstruments(List<Instrument> instruments) {
+		this.instruments = instruments;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	@XmlElement
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+}
