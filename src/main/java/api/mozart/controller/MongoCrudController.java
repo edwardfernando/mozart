f773c11fc7fdf39f957717153c16e297dff7f7e6
Edward Fernando <edward.fer@gmail.com>
2013-08-17 09:33:52 +0700

    Add Crud with mongoDB

---
+++ src/main/java/api/mozart/controller/MongoCrudController.java
@@ -0,0 +1,42 @@
+package api.mozart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import api.mozart.model.Instrument;
+import api.mozart.service.InstrumentService;
+
+@Controller
+@RequestMapping("/crud/instrument")
+public class MongoCrudController {
+
+	@Autowired
+	private InstrumentService service;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String list(ModelMap model) {
+		model.addAttribute("instrumentList", service.list());
+		return "instrument";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(@ModelAttribute Instrument ins, ModelMap model) {
+		if (StringUtils.hasText(ins.getId())) {
+			service.update(ins);
+		} else {
+			service.add(ins);
+		}
+		return "redirect:/crud/instrument/";
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public String delete(@ModelAttribute Instrument ins, ModelMap model) {
+		service.delete(ins);
+		return "redirect:/crud/instrument/";
+	}
+}
--- src/main/java/api/mozart/model/Instrument.java
+++ src/main/java/api/mozart/model/Instrument.java
@@ -3,11 +3,25 @@ package api.mozart.model;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 @XmlRootElement(name = "instrument")
 public class Instrument {
+	@Id
+	String id;
 	String name;
 	String key;
 	int quality;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
