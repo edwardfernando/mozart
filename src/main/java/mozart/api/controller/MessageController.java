9326da3315e46b656be57af739cfc6bb13cb6904
Edward Fernando <edward.fer@gmail.com>
2013-08-23 19:02:25 +0700

    New controller structure

--- src/main/java/mozart/api/controller/MessageController.java
+++ src/main/java/mozart/api/controller/MessageController.java
@@ -1,25 +1,39 @@
 package mozart.api.controller;
 
-import mozart.api.model.Message;
-import mozart.api.service.MessageService;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import mozart.api.model.Message;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/message")
-public class MessageController {
+public class MessageController implements mozart.api.controller.Controller<Message> {
 
-	@Autowired
-	private MessageService service;
+	@Override
+	public List<Message> loadAll() {
+		return null;
+	}
 
-	@RequestMapping("/{id}")
-	@ResponseBody
-	public Message loadById(@PathVariable String id) {
-		return new Message();
+	@Override
+	public Message loadById(String id) {
+		return null;
+	}
+
+	@Override
+	public void save(Message model) {
+
+	}
+
+	@Override
+	public Message update(Message model) {
+		return null;
+	}
+
+	@Override
+	public void delete(Message model) {
+
 	}
 
 }
