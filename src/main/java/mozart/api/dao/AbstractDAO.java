70cb41344eb977a5fb3a0c21b10925dfefdae312
Edward Fernando <edward.fer@gmail.com>
2013-08-29 10:07:14 +0700

    DAO skeleton

--- src/main/java/mozart/api/dao/AbstractDAO.java
+++ src/main/java/mozart/api/dao/AbstractDAO.java
@@ -2,23 +2,35 @@ package mozart.api.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
 public abstract class AbstractDAO<T> {
+
+	@Autowired
+	private MongoTemplate template;
 
 	protected abstract Class<T> getModel();
 
 	public List<T> loadAll() {
-		return null;
+		return template.findAll(getModel(), getModel().getSimpleName());
 	}
 
-	public T save(T model) {
-		return null;
+	public T loadById(String id) {
+		Query query = new Query(Criteria.where("id").is(id));
+		return template.findOne(query, getModel(), getModel().getSimpleName());
 	}
 
-	public T update(T model) {
-		return null;
+	public void save(T model) {
+		if (!template.collectionExists(getModel())) {
+			template.createCollection(getModel());
+		}
+		template.save(model, getModel().getSimpleName());
 	}
 
-	public T delete(T model) {
-		return null;
+	public void delete(T model) {
+		template.remove(model, getModel().getSimpleName());
 	}
 }
