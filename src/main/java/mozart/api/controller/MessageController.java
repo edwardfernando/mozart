01e95cb8dd0dfb2176bb959e8e39493b50b8f846
Edward Fernando <edward.fer@gmail.com>
2013-08-29 10:06:49 +0700

    Controller sample for CRUD operation

--- src/main/java/mozart/api/controller/MessageController.java
+++ src/main/java/mozart/api/controller/MessageController.java
@@ -1,27 +1,69 @@
 package mozart.api.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import mozart.api.model.Message;
+import mozart.api.service.MessageService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/message")
 public class MessageController extends mozart.api.controller.Controller<Message> {
 
+	@Autowired
+	private MessageService service;
+
 	@RequestMapping("/")
 	@Override
 	public List<Message> loadAll() {
 		System.out.println("hoiii");
-		return null;
+		return new ArrayList<Message>();
 	}
 
 	@Override
-	public Message loadById(@PathVariable String id) {
-		return null;
+	@ResponseBody
+	@RequestMapping("/{id}")
+	public Message loadById(@PathVariable("id") String id) {
+		Message m = service.loadById(id);
+		System.out.println("Result from mongo : " + m.getId() + "/" + m.getMessage());
+		return m;
+	}
+
+	@ResponseBody
+	@RequestMapping("/testsave")
+	public Message testSave() {
+		Message m = new Message();
+		m.setDate(new Date());
+		m.setMessage("Hello, Folks! Apa kabar?");
+
+		service.save(m);
+
+		return m;
+	}
+
+	@ResponseBody
+	@RequestMapping("/testupdate/{id}")
+	public Message testUpdate(@PathVariable("id") String id) {
+		Message m = service.loadById(id);
+
+		m.setMessage("Message baru coiiiiiiiii ");
+		service.save(m);
+		return m;
+	}
+
+	@ResponseBody
+	@RequestMapping("/testdelete/{id}")
+	public Message testDelete(@PathVariable("id") String id) {
+		service.delete(service.loadById(id));
+
+		return new Message();
 	}
 
 	@Override
