11338cbd9a61f23839ace7892e920c2e8fddbfc1
Edward Fernando <edward.fer@gmail.com>
2013-08-29 10:07:27 +0700

    Service layer skeleton

--- src/main/java/mozart/api/service/MessageService.java
+++ src/main/java/mozart/api/service/MessageService.java
@@ -2,12 +2,17 @@ package mozart.api.service;
 
 import java.util.List;
 
+import mozart.api.dao.MessageDAO;
 import mozart.api.model.Message;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageService implements Service<Message> {
+
+	@Autowired
+	private MessageDAO dao;
 
 	@Override
 	public List<Message> loadAll() {
@@ -16,22 +21,23 @@ public class MessageService implements Service<Message> {
 
 	@Override
 	public Message loadById(String id) {
-		return null;
+		return dao.loadById(id);
 	}
 
 	@Override
 	public void save(Message model) {
-
-	}
-
-	@Override
-	public Message update(Message model) {
-		return null;
+		dao.save(model);
 	}
 
 	@Override
 	public void delete(Message model) {
+		dao.delete(model);
+	}
 
+	@Override
+	public Message update(Message model) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
