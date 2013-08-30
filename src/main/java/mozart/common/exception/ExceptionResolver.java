4486dea9f35e124e202aa2eb37a36cf8c2dc24bd
Edward Fernando <edward.fer@gmail.com>
2013-08-30 09:39:05 +0700

    Add exception handling capabilities

--- src/main/java/mozart/common/exception/ExceptionResolver.java
+++ src/main/java/mozart/common/exception/ExceptionResolver.java
@@ -1,5 +1,35 @@
 package mozart.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ExceptionResolver {
 
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public ErrorMessage handleException(Exception ex) {
+		return new ErrorMessage(ex.getMessage());
+	}
+
+	/*
+	 * @ExceptionHandler
+	 * 
+	 * @ResponseStatus(HttpStatus.BAD_REQUEST)
+	 * 
+	 * @ResponseBody public ErrorMessage
+	 * handleException(MethodArgumentNotValidException ex) { List<FieldError>
+	 * fieldErrors = ex.getBindingResult().getFieldErrors(); List<ObjectError>
+	 * globalErrors = ex.getBindingResult().getGlobalErrors(); List<String>
+	 * errors = new ArrayList<String>(fieldErrors.size() + globalErrors.size());
+	 * String error; for (FieldError fieldError : fieldErrors) { error =
+	 * fieldError.getField() + ":" + fieldError.getDefaultMessage();
+	 * errors.add(error); } for (ObjectError objectError : globalErrors) { error
+	 * = objectError.getObjectName() + ":" + objectError.getDefaultMessage();
+	 * errors.add(error); } return new ErrorMessage(errors); }
+	 */
 }
--- src/main/java/mozart/common/exception/MozartException.java
+++ src/main/java/mozart/common/exception/MozartException.java
@@ -1,9 +1,10 @@
 package mozart.common.exception;
 
 public class MozartException extends Exception {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
+	public MozartException(String message) {
+		super(message);
+	}
 }
