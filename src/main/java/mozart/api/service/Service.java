ef0e7e8e87bcd191d39773770b9b84ad06c975eb
Edward Fernando <edward.fer@gmail.com>
2013-09-06 16:33:16 +0700

    New abtract service that acted as base service

--- src/main/java/mozart/api/service/Service.java
+++ src/main/java/mozart/api/service/Service.java
@@ -2,15 +2,46 @@ package mozart.api.service;
 
 import java.util.List;
 
-public interface Service<T> {
+import mozart.api.dao.AbstractDAO;
+import mozart.common.exception.MozartException;
 
-	public List<T> loadAll();
+public abstract class Service<T> {
 
-	public T loadById(String id);
+	protected abstract AbstractDAO<T> getDao();
 
-	public void save(T model);
+	public List<T> loadAll() {
+		return getDao().loadAll();
+	}
 
-	public T update(T model);
+	public T loadById(String id) throws MozartException {
+		T obj = getDao().loadById(id);
+		if (obj == null) {
+			throw new MozartException("Resource '" +
+			                          getDao().getModel().getSimpleName().toLowerCase() +
+			                          "' with identifier '" +
+			                          id +
+			                          "' is not found");
+		}
+		return obj;
+	}
 
-	public void delete(T model);
+	public void save(T model) {
+		getDao().save(model);
+	}
+
+	public void update(T model) {
+		save(model);
+	}
+
+	public void delete(String id) throws MozartException {
+		T obj = getDao().loadById(id);
+		if (obj == null) {
+			throw new MozartException("Resource '" +
+			                          getDao().getModel().getSimpleName().toLowerCase() +
+			                          "' with identifier '" +
+			                          id +
+			                          "' is not found");
+		}
+		getDao().delete(obj);
+	}
 }
