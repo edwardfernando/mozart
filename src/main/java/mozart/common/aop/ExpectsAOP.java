17e8bf917010dc717faaa0c46dac6a673be19f6e
Edward Fernando <edward.fer@gmail.com>
2013-09-10 01:31:22 +0700

    Package for AOP class

---
+++ src/main/java/mozart/common/aop/ExpectsAOP.java
@@ -0,0 +1,27 @@
+package mozart.common.aop;
+
+import javax.servlet.http.HttpServletRequest;
+
+import mozart.common.annotation.Expects;
+import mozart.common.exception.MozartException;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class ExpectsAOP {
+
+	@Before("@annotation(expects) && args(request,..)")
+	public void inspect(Expects expects, HttpServletRequest request) throws MozartException {
+		for (String expectedValue : expects.value()) {
+			if (request.getParameter(expectedValue) == null) {
+				throw new MozartException("Expected " +
+				                          request.getMethod() +
+				                          " parameter '" +
+				                          expectedValue +
+				                          "' at URI " +
+				                          request.getPathInfo());
+			}
+		}
+	}
+}
