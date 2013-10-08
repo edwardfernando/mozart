f9d26de18e65ddd749e55d7ce6ecfb80699080de
Edward Fernando <edward.fer@gmail.com>
2013-10-08 09:28:32 +0700

    Modify abstract dao to support Hibernate

--- src/main/java/mozart/api/dao/AbstractDAO.java
+++ src/main/java/mozart/api/dao/AbstractDAO.java
@@ -5,36 +5,22 @@ import java.util.List;
 import mozart.common.pagination.Filterable;
 import mozart.common.pagination.FilterableQuery;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-
 public abstract class AbstractDAO<T> implements Filterable {
-
-	@Autowired
-	private MongoTemplate template;
 
 	public abstract Class<T> getModel();
 
 	public List<T> loadAll() {
-		return template.findAll(getModel(), getModel().getSimpleName());
+		return null;
 	}
 
 	public T loadById(String id) {
-		Query query = new Query(Criteria.where("id").is(id));
-		return template.findOne(query, getModel(), getModel().getSimpleName());
+		return null;
 	}
 
 	public void save(T model) {
-		if (!template.collectionExists(getModel())) {
-			template.createCollection(getModel());
-		}
-		template.save(model, getModel().getSimpleName());
 	}
 
 	public void delete(T model) {
-		template.remove(model, getModel().getSimpleName());
 	}
 
 	@SuppressWarnings("unchecked")
